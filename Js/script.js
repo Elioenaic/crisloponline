af8d01fc957344d2dcededc554b61f1e4ef5bc60
Elioenai Caballero <elioenai@hotmail.com.ar>
2025-06-09 20:21:37 -0300

    Carga inicial de archivos

---
+++ Js/script.js
@@ -0,0 +1,65 @@
+//console.log("Hola!");
+ 
+
+/*let mensaje = "Soy un mensaje en una variable";
+console.log(mensaje);
+
+mensaje = "Saludos!"
+console.log(mensaje);
+
+let numeroA = ""
+console.log(numeroA)
+
+const IVA = 21
+IVA = 20
+console.log(IVA)
+
+let socio = false*/
+//let precio = parseInt("300 pesos") 
+/*el color de los números define si es una cadena de texto oun valor numérico*/
+//console.log(precio)
+//let precio = parseFloat("300 pesos")
+
+/*let numeroA = 50
+let numeroB = 100
+let resultado = numeroA + numeroB
+console.log(resultado)*/
+
+/*let numeroA = parseInt (prompt("Ingrese un numero: "))
+let numeroB = parseInt (prompt("Ingrese un numero: "))
+let resultado = numeroA + numeroB
+console.log(resultado)*/
+
+/*let nombre = Elioenai
+let apellido = Caballero
+let saludo = nombre + apellido
+console.log(saludo)*/
+
+/*let numeroA = parseInt (prompt("Ingrese un numero: "))
+let numeroB = parseInt (prompt("Ingrese un numero: "))
+let resultado = numeroA + numeroB
+alert(resultado)*/
+
+/*let resultado = confirm("¿Desea salir?")*/
+
+/*let numeroA = parseInt(prompt("Ingrese un numero: "));
+let numeroB = parseInt(prompt("Ingrese un numero: "));
+let resultado = numeroA + numeroB
+document.writeln(resultado);*/
+
+let edad = parseInt(prompt("Ingrese su edad: "))
+
+if (edad >= 18) {
+    alert("Sos mayor de edad.");
+} else {
+    alert("Sos menor de edad.");
+}
+
+
+let nota = parseInt(prompt("Ingrese su primera nota: "))
+
+if (nota == 10) {
+    alert("Excelente");
+} else if {
+    
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+
+Web CrisLOP
+===========
+
+Página web creadacomo ejercitación del curso de Front-End de Talento Tech 2025.
+Sin fines comerciales.
